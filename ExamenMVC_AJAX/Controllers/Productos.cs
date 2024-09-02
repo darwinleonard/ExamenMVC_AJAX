e97f3e5e7a396ad90cfc47d9389a0d46cb9e47f5
darwinleonardsl@gmail.com <darwinleonardsl@gmail.com>
2024-09-02 07:01:21 -0500

    Agregar archivos de proyecto.

---
+++ ExamenMVC_AJAX/Controllers/Productos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExamenMVC_AJAX.Controllers
+{
+    public class Productos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
